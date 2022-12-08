9b1e32e21a379c93589a6768832b7bb8da625518
angelakuo6291 <119844775+liveabcrd1@users.noreply.github.com>
2022-12-08 15:33:23 +0800

    20221208-show 下載路徑

--- js/cnndw.js
+++ js/cnndw.js
@@ -1,6 +1,6 @@
 $(function(){
 	//* Get Json Data-- ABC
-	getKindJson('src/','livecdupd','CNN');		
+	gethrefKindJson('src/','livecdupd','CNN');		
 })
 
 function btnClick(evt) {
@@ -12,6 +12,7 @@ function btnClick(evt) {
 	} else {
 		file += obj.dataset.x32;
 	}	
+	//obj.href=file;
 	window.location=file;	
 }
 
@@ -35,10 +36,51 @@ function getKindJson(fpath, fname, fkind) {
 					str = "<tr id='"+ kind+ "-" + rowid + "'>" + 
 					"<td>" + field[jj].date + "</td> <td> <p> " + field[jj].product + "</p></td>" +
 					"<td> <p>" + field[jj].desc +
-					"</p> </td> <td><a class='x32btn btn w3-win8-brown-l4 w3-hover-pale-green' onclick='btnClick(event)' " + 
+					"</p> </td> <td><a class='x32btn btn w3-win8-brown-l4 w3-hover-pale-green' OnClick='btnClick(event)' " + 
 					" data-x32='" + x32file + "' data-url='" + url + "' data-folder='" + field[jj].Folder + 
 					"' download ><span class='icon-download'></span> </a></td>" +					
 					"<td> <a class='x64btn btn w3-win8-brown-l4 w3-hover-pale-green'  onclick='btnClick(event)' " + 
+					"' data-x64='" + x64file + "' data-url='" + url + "' data-folder='" + field[jj].Folder + 					
+					"' download> <span class='icon-download'></span></a> </td> " + 
+					" </tr>";
+					$(obj).append(str);
+				  }
+			  
+				console.log(kind + " -- " + field.length);	
+				return false; // 等於break
+			} else{
+				console.log(kind + " -- " + field.length);	
+				return; 
+			}									
+		});
+	  });
+  }
+
+  function gethrefKindJson(fpath, fname, fkind) {
+	fileName = fpath + fname + '.json';   	
+	// read JSON object from file
+	$.getJSON(fileName,function(result){         
+	   json_obj = result;
+		$.each(result, function(kind, field){   
+			if(kind == fkind){
+				var obj = "#dw-body";
+				var rowid=0;		
+				var url ="";
+				var x32file="";
+				var x64file="";
+				for(let jj=0; jj<field.length; jj++) {
+					rowid = jj+1;					
+					url=field[jj].URL;
+					fl=field[jj].Folder;					
+					x32file=field[jj].x32bits;
+					x64file=field[jj].x64bits;
+					str = "<tr id='"+ kind+ "-" + rowid + "'>" + 
+					"<td>" + field[jj].date + "</td> <td> <p> " + field[jj].product + "</p></td>" +
+					"<td> <p>" + field[jj].desc +
+					"</p> </td> <td><a class='x32btn btn w3-win8-brown-l4 w3-hover-pale-green' href='"+ url + fl + "/"+ x32file + "' " + 
+					" data-x32='" + x32file + "' data-url='" + url + "' data-folder='" + field[jj].Folder + 
+					"' download ><span class='icon-download'></span> </a></td>" +					
+					"<td> <a class='x64btn btn w3-win8-brown-l4 w3-hover-pale-green' href='"+ url + fl + "/"+ x32file + "' " + 
 					"' data-x64='" + x64file + "' data-url='" + url + "' data-folder='" + field[jj].Folder + 					
 					"' download> <span class='icon-download'></span></a> </td> " + 
 					" </tr>";
